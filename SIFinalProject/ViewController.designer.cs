1eb6f31bdbbd4be7170825156b023988f51d41cd
juanmapf97 <juanmapf97@gmail.com>
2017-04-07 11:19:55 -0500

    Added classes for Reservation and Vehicle, and open file button functionality

--- SIFinalProject/ViewController.designer.cs
+++ SIFinalProject/ViewController.designer.cs
@@ -1,17 +1,24 @@
-﻿// WARNING
+// WARNING
 //
 // This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace SIFinalProject
 {
-	[Register("ViewController")]
+	[Register ("ViewController")]
 	partial class ViewController
 	{
-		void ReleaseDesignerOutlets()
+		[Action ("ClickedButton:")]
+		partial void ClickedButton (Foundation.NSObject sender);
+
+		[Action ("OpenFile:")]
+		partial void OpenFile (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
 		{
 		}
 	}
